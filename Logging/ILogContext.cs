85b3b7fe71df80a7d4d661396ab37c9aef3eb231
Рамиль Вахитов <vnrameel@hotmail.com>
2018-05-26 17:38:40 +0300

    Logging api

---
+++ Logging/ILogContext.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace Logging
+{
+    public interface ILogContext
+    {
+        [NotNull]
+        ILogContext AddProperty([NotNull] string name, [NotNull] object value, bool destructure = false);
+    }
+}
